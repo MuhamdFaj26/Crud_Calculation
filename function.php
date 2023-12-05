69e5bb900acc02352a9ea18b0f6b5b3a88ed4d19
MuhamdFaj26 <muhammad.izzulhaq@students.amikom.ac.id>
2023-12-05 11:34:23 +0700

    p

--- function.php
+++ function.php
@@ -1,14 +1,26 @@
 <?php
 
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbName = "calculation";
 
-$connection = new mysqli($servername, $username, $password);
+$connection = new mysqli($servername, $username, $password, $dbName);
 
-if (!$connection){
-    return "salah";
+if ($connection->connect_error){
+    die("Connection Failde :" .$connection->connect_error);
+}else{
+    echo "Connection Succes";
+}
+
+$new = "INSERT INTO user (Nama, Alamat, No_HP) VALUES ('FAJRUL', 'ZUL', '082221480074')";
+
+
+if($connection->query($new) === true){
+    echo "SUCCES";
+}else{
+    echo "error:" .$new. "<br>" .$connection->error;
 }
 
 ?>
